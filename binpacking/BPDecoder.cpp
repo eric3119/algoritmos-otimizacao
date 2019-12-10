f11b1716a0e3da9c08724d264ccf54f2afc1a6ec
eric3119 <esc2@ic.ufal.br>
2019-12-10 10:49:37 -0300

    add DFTRC

--- binpacking/BPDecoder.cpp
+++ binpacking/BPDecoder.cpp
@@ -1,34 +1,82 @@
 #include "BPDecoder.h"
 
-int boxes[5][2] = {
-	{2, 2},
-	{2, 2},
-	{2, 2},
-	{2, 2},
-	{2, 2}
-};
-
 BPDecoder::BPDecoder() {}
 
 BPDecoder::~BPDecoder() {}
 
-double BPDecoder::DFTRC(const std::list<unsigned> &permutation) const{
+double BPDecoder::DFTRC(std::list<unsigned> &permutation) const{
 
-	//PriorityQueue
+	std::priority_queue < Space > pq;
 
+	int bin_w = 10, bin_h = 10;
+	int NB = 1;
+
+	pq.push(Space(bin_w, bin_h, NB));
+
+	std::vector< std::pair<int, int> > boxes;
+	
+	std::vector<int> bin_capacity;
+	bin_capacity.push_back(bin_w * bin_h);
+
+	boxes.push_back(std::make_pair(2,2));
+	boxes.push_back(std::make_pair(2,2));
+	boxes.push_back(std::make_pair(2,2));
+	boxes.push_back(std::make_pair(2,2));
+	boxes.push_back(std::make_pair(2,2));
+	boxes.push_back(std::make_pair(2,2));
+	boxes.push_back(std::make_pair(2,2));
+	boxes.push_back(std::make_pair(2,2));
+	boxes.push_back(std::make_pair(2,2));
+	boxes.push_back(std::make_pair(2,2));
+
+	for (std::list<unsigned>::iterator it=permutation.begin(); it != permutation.end(); ++it){
+		Space ems = pq.top();
+		pq.pop();
+
+		int box_w = boxes[*it].first, 
+			box_h = boxes[*it].second;
+
+		if(ems.width >= box_w && ems.height >= box_h){
+			pq.push(Space(ems.width - box_w, ems.height, ems.bin_number));
+			pq.push(Space(ems.width, ems.height - box_h, ems.bin_number));
+
+			bin_capacity[ems.bin_number - 1] -= box_w * box_h;
+
+		} else {
+			NB++;
+			pq.push(Space(bin_w - box_w, bin_h, NB));
+			pq.push(Space(bin_w, bin_h - box_h, NB));
+
+			bin_capacity.push_back(bin_w * bin_h - box_w * box_h);
+		}
+
+		double least_load = std::numeric_limits<double>::max();
+		for (int i = 0; i < NB; i++){
+			if(bin_capacity[i] < least_load){
+				least_load = bin_capacity[i];
+			}
+		}
+
+		return NB + least_load / (bin_w * bin_h);
+	}
+
+
+	
+	std::cout << std::endl;
+	
+
+	return 0;
 }
 
 // Runs in O(n \log n):
 double BPDecoder::decode(const std::vector<double> &chromosome) const
 {
-	double myFitness = 0.0;
 	typedef std::pair<double, unsigned> ValueKeyPair;
 	std::vector<ValueKeyPair> rank(chromosome.size());
 
 	for (unsigned i = 0; i < chromosome.size(); ++i)
 	{
 		rank[i] = ValueKeyPair(chromosome[i], i);
-		myFitness += (double(i + 1) * chromosome[i]);
 	}
 
 	// Here we sort 'permutation', which will then produce a permutation of [n]
@@ -42,8 +90,6 @@ double BPDecoder::decode(const std::vector<double> &chromosome) const
 		permutation.push_back(i->second);
 	}
 	
-	// myFitness = DFTRC(permutation);
-
-	// Return the fitness:
-	return myFitness;
+	// return fitness
+	return DFTRC(permutation);
 }
--- binpacking/BPDecoder.h
+++ binpacking/BPDecoder.h
@@ -4,6 +4,22 @@
 #include <list>
 #include <vector>
 #include <algorithm>
+#include <iostream>
+#include <queue>
+#include <limits>
+
+typedef struct Space{
+    int size;
+    int width;
+    int height;
+	int bin_number;
+
+    Space(int w, int h, int bin_number) : size(w * h), width(w), height(h), bin_number(bin_number){}
+
+    bool operator<(const struct Space& other) const {
+        return size < other.size;
+    }
+}Space;
 
 class BPDecoder
 {
@@ -13,7 +29,7 @@ public:
 
 	// Decode a chromosome, returning its fitness as a double-precision floating point:
 	double decode(const std::vector<double> &chromosome) const;
-	double DFTRC(const std::list<unsigned> &permutation) const;
+	double DFTRC(std::list<unsigned> &permutation) const;
 private:
 };
 
